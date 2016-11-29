e1fc4b823d56219bb8286600157eb74fb920cb3c
samestrada <estradas@oregonstate.edu>
2016-11-29 14:52:35 -0800

    Update server.js

--- final/server.js
+++ final/server.js
@@ -1,1 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var planets = require('./planets');
+var app = express();
 
+var port = process.env.PORT || 3000;
+                         
+app.engine('handlebars', exphbs({default layout : 'main'}))
+
+                         
