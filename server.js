b3f2f0159ea1c313ece4530a7016bcef02172a8c
jeffreykat <jeffreyk@oregonstate.edu>
2016-12-01 16:43:48 -0800

    Updated server

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-/*server.js*/
-var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
@@ -8,7 +6,42 @@ var planets = require('./planets');
 var app = express();
 
 var port = process.env.PORT || 3000;
-                         
-app.engine('handlebars', exphbs({default layout : 'main'}))
 
-                         
+// Use Handlebars as the view engine for the app.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+   res.render('index-page',{
+      pageTitle: ''
+   });
+});
+
+app.get('/notes/:planet', function(req, res) {
+   var user = req.params.planet.toString();
+   var reqUser = usersData[user];
+   if(user){
+      console.log(reqUser.name);
+      res.status(200).render('planet',{
+	 pageTitle: ''
+      });
+   }
+   else {
+      next();
+   }
+});
+
+app.get('*', function(req, res) {
+   res.render('404-page', {
+      pageTitle: '404'
+   });
+});
+
+
+// Listen on the specified port.
+app.listen(port, function () {
+   console.log("== Listening on port", port);
+});
+
