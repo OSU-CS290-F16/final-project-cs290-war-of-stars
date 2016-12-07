67b381d876ba58c0992f64543df1dd56d136540b
PadraigGillen <gillenp@oregonstate.edu>
2016-12-07 14:13:57 -0800

    add basic mongo connection

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var planets = require('./final/planets.json');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
 var port = process.env.PORT || 3000;
+
+// use modified class Mongo code to set up connection
+var mongoHost = process.env.MONGO_HOST || "classmongo.engr.oregonstate.edu";
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER || "cs290_gillenp";
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB || "cs290_gillenp";
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+var mongoDB;
 
 // Use Handlebars as the view engine for the app.
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -46,8 +56,16 @@ app.get('*', function(req, res) {
    });
 });
 
+// set up MongoDB connection and start server
+MongoClient.connect(mongoURL, function (err, db) {
+   if (err) {
+      console.log("== Unable to make connection to MongoDB Database.");
+      throw err;
+   }
+   mongoDB = db; // successful connection
 
-// Listen on the specified port.
-app.listen(port, function () {
-   console.log("== Listening on port", port);
+   // express now listens on the specified port
+   app.listen(port, function () {
+      console.log("== Listening on port", port);
+   });
 });
