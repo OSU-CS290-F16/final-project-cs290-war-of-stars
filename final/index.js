e7f3849043f32296245d1f9c5d86da0d6720aa52
jeffreykat <jeffreyk@oregonstate.edu>
2016-12-07 14:42:23 -0800

    events and style updates

--- final/index.js
+++ final/index.js
@@ -2,4 +2,46 @@
 * login
 * entering planets
 * leaving planets
-* closing game*/
+* closing game
+* */
+
+var loginButton = document.getElementsByClassName('login-button');
+for(var i = 0; i < loginButton.length; i++){
+   loginButton[i].addEventListener('click', handleLoginClick);
+}
+
+var planet = document.getElementsByClassName('planet-click');
+for(var i = 0; i < planet.length; i++){
+   planet[i].addEventListener('click', handlePlanetClick);
+}
+
+var homeButton = document.getElementsByClassName('home');
+for(var i = 0; i < homeButton.length; i++){
+   homeButton[i].addEventListener('click', handleHomeClick);
+}
+
+var logoutButton = document.getElementsByClassName('logout');
+for(var i = 0; i < logoutButton.length; i++){
+   logoutButton[i].addEventListener('click', handleLogoutClick);
+}
+
+function handleLoginClick(event){
+   var username = document.getElementsByTagName("input")[0].value;
+   console.log(username);
+   location.replace("/index");
+}
+
+function handlePlanetClick(event){
+   /*enter planet page*/
+   var planetName = event.target.alt;
+   location.replace("/" + planetName);
+}
+
+function handleHomeClick(event){
+   /*add credits to user for collected bounties*/
+}
+
+function handleLogoutClick(event){
+   /*block home button in nav bar*/
+   /*store user data and user*/
+}
